3f90efe68f0b5f7b6121755e6b9ad61ace715d40
Eduard <eduardkarimov.rb@gmail.com>
2023-12-23 23:29:58 +0500

    add new futures

---
+++ Tracker/CategoryCell.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryCell.swift
+//  Tracker
+//
+//  Created by Eduard Karimov on 23/12/2023.
+//
+
+import Foundation
---
+++ Tracker/TrackerViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerViewCell.swift
+//  Tracker
+//
+//  Created by Eduard Karimov on 23/12/2023.
+//
+
+import Foundation
